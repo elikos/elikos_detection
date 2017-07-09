40cdcd7629270db8896b6e10ef3c8375ea9bc8bf
elikos-user <directeur@elikos.polymtl.ca>
2017-07-09 15:26:07 -0400

    Preprocessing integration

---
+++ src/elikos_context_utils/QuadState.h
@@ -0,0 +1,55 @@
+#ifndef QUAD_STATE_H
+#define QUAD_STATE_H
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <tf/transform_listener.h>
+
+#include "tf/tf.h"
+
+class QuadState
+{
+public:
+    QuadState() = default;
+    QuadState(const std::string& cameraFrame);
+    ~QuadState() = default;
+
+    inline void setCameraFrame(const std::string& cameraFrame);
+
+    void update(ros::Time stamp);
+
+    ros::Time getTimeStamp() const;
+    tf::StampedTransform getOrigin2Fcu() const;
+    tf::StampedTransform getFcu2Camera() const;
+
+private:
+    std::string cameraFrame_;
+
+    tf::TransformListener tfListener_;
+
+    tf::StampedTransform origin2fcu_;
+    tf::StampedTransform fcu2camera_;
+    ros::Time timeStamp_;
+};
+
+inline ros::Time QuadState::getTimeStamp() const
+{
+    return timeStamp_;
+}
+
+inline tf::StampedTransform QuadState::getOrigin2Fcu() const
+{
+    return origin2fcu_;
+}
+
+inline tf::StampedTransform QuadState::getFcu2Camera() const
+{
+    return fcu2camera_;
+}
+
+inline void QuadState::setCameraFrame(const std::string& cameraFrame)
+{
+    cameraFrame_ = cameraFrame;
+}
+
+#endif // QUAD_STATE_H
