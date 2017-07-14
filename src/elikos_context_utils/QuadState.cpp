56b0746c862cf2f5524ed060147aa482e3df07ec
elikos-user <directeurs@polymtl.ca>
2017-07-14 19:19:36 -0400

    Merge relative-localization into master

--- src/elikos_context_utils/QuadState.cpp
+++ src/elikos_context_utils/QuadState.cpp
@@ -5,19 +5,23 @@ QuadState::QuadState(const std::string& cameraFrame)
 {
 }
 
-void QuadState::update(ros::Time stamp)
+bool QuadState::update(ros::Time stamp)
 {
     timeStamp_ = stamp;
+    bool success = false;
     try {
         tfListener_.waitForTransform("elikos_arena_origin", "elikos_fcu",  stamp, ros::Duration(1.0));
         tfListener_.lookupTransform("elikos_arena_origin", "elikos_fcu", stamp, origin2fcu_);
 
         tfListener_.waitForTransform("elikos_fcu", cameraFrame_, stamp, ros::Duration(1.0));
         tfListener_.lookupTransform("elikos_fcu", cameraFrame_, stamp, fcu2camera_);
+        success = true;
     }
     catch (tf::TransformException e)
     {
-        ROS_ERROR("%s", e.what());
+        ROS_ERROR(e.what());
+
     }
+    return success;
 }
 
